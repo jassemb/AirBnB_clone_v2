7e96e849acbd38d765d811b3040e5b0a720be13a
jassemb <benalijassem201@gmail.com>
2022-01-13 14:12:33 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,5 +12,3 @@ chown ubuntu:ubuntu -hR /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 
-
-jas
