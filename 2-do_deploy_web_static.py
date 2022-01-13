7c563d73774f4835ef61139b15c358c5983ba81d
jassemb <benalijassem201@gmail.com>
2022-01-13 23:56:10 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ archive to the web servers
 """
 from fabric.api import put, run, env
 from fabric.api import local
-from os.path import isdir ,exists
+from os.path import isdir, exists
 env.hosts = ['34.74.244.100', '35.243.249.147']
 
 
@@ -26,5 +26,5 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except BaseException:
         return False
